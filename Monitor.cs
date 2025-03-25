db7096df44c11a70c0c3c3de0f5705721f797320
Vitas <vitas.mellilo@gmail.com>
2025-03-25 15:48:22 -0300

    adicionado processamento paralelo

--- Monitor.cs
+++ Monitor.cs
@@ -10,27 +10,34 @@ namespace PoryGuard
 {
     class Monitor
     {
+        private int id;
         private string nome;
         private int frameRate;
         private int altura;
         private int largura;
+        private int boundsX;
+        private int boundsY;
         
         public Monitor() 
         { 
             Screen screen = Screen.AllScreens[0];
             ManagementObjectSearcher searcher = new ManagementObjectSearcher(@"SELECT * FROM Win32_VideoController");
-
+            id = 0;
             foreach (ManagementObject obj in searcher.Get())
             {
                 frameRate = int.Parse(obj["CurrentRefreshRate"].ToString());
                 break;
+                id++;
             }
             nome = screen.DeviceName;
             altura = screen.Bounds.Height;
             largura = screen.Bounds.Width;
-            Console.WriteLine($"Monitor: {nome}");
-            Console.WriteLine($"Taxa de atualização: {frameRate} Hz");
-            Console.WriteLine($"Resolução: {largura}x{altura}");
+            boundsX = screen.Bounds.X;
+            boundsY = screen.Bounds.Y;
+        }
+        public int GetId()
+        { 
+            return id; 
         }
         public string GetNome()
         {
@@ -39,6 +46,22 @@ namespace PoryGuard
         public int GetFrameRate()
         {
             return frameRate;
+        }
+        public int GetAltura()
+        {
+            return altura;
+        }
+        public int GetLargura()
+        {
+            return largura;
+        }
+        public int GetBoundsX()
+        {
+            return boundsX;
+        }
+        public int GetBoundsY()
+        {
+            return boundsY;
         }
     }
 }
--- PoryGuard.cs
+++ PoryGuard.cs
@@ -9,35 +9,88 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Drawing.Imaging;
+using System.Runtime.InteropServices;
+using SharpDX;
+using SharpDX.Direct3D11;
+using SharpDX.DXGI;
+using SharpDX.Mathematics.Interop;
+using System.Threading;
 
 namespace PoryGuard
 {
     public partial class PoryGuard: Form
     {
+        int altura;
+        int largura;
+        int frameRate;
+        int boundsX;
+        int boundsY;
+        int bytesPerPixel = 4;
+        int stride;
+        int totalBytes;
+        int offset;
+        Size size;
+        short[,,,] pixels;
         public PoryGuard()
-        { 
+        {
             InitializeComponent();
-            
-            ManagementObjectSearcher searcher = new ManagementObjectSearcher(@"SELECT * FROM Win32_VideoController");
-            /*
-            foreach (Screen screen in Screen.AllScreens)
-            {
-                Console.WriteLine($"Monitor: {screen.DeviceName}");
-                Console.WriteLine($"Resolução: {screen.Bounds.Width}x{screen.Bounds.Height}");
-                Console.WriteLine($"Principal: {screen.Primary}");
-                Console.WriteLine($"Área de Trabalho: {screen.WorkingArea}");
-                Console.WriteLine($"Escala: {screen.Bounds.Width / (double)screen.WorkingArea.Width * 100}%");
-                Console.WriteLine(new string('-', 40));
-                var bitmap = new Bitmap(screen.Bounds.Width, screen.Bounds.Height);
-                Graphics.FromImage(bitmap).CopyFromScreen(screen.Bounds.X, screen.Bounds.Y, 0, 0, screen.Bounds.Size);
-                Color pixelColor = bitmap.GetPixel(100, 100);
-                Console.WriteLine("Cor do pixel na coordenada (100, 100):");
-                Console.WriteLine($"- Red: {pixelColor.R}");
-                Console.WriteLine($"- Green: {pixelColor.G}");
-                Console.WriteLine($"- Blue: {pixelColor.B}");
-                Console.WriteLine($"- Alpha: {pixelColor.A}");
-            }*/
             Monitor monitor = new Monitor();
+            ColetaBitmap(monitor);
         }
+        private void ColetaBitmap(Monitor monitor)
+        {
+            altura = monitor.GetAltura();
+            largura = monitor.GetLargura();
+            frameRate = monitor.GetFrameRate();
+            boundsX = monitor.GetBoundsX();
+            boundsY = monitor.GetBoundsY();
+            List<Thread> coletor = new List<Thread>();
+            size = new Size(largura, altura);
+            pixels = new short[largura, altura, frameRate, 4];
+            for (int i = 0; i < frameRate; i++)
+            {
+                int aux = i;
+                Thread t = new Thread(() => ColetaFrame(aux));
+                coletor.Add(t);
+                t.IsBackground = true;
+                t.Start();
+            }
+            Console.WriteLine("Acabou");
+        }
+        public void ColetaFrame()
+        {
+
+        }
+        public void ColetaFrame(int i)
+        {
+            BitmapData bitmapData;
+            using (Bitmap bitmap = new Bitmap(largura, altura))
+            {
+                using (Graphics g = Graphics.FromImage(bitmap))
+                {
+                    g.CopyFromScreen(boundsX, boundsY, 0, 0, size);
+                }
+                bitmapData = bitmap.LockBits(new Rectangle(0, 0, bitmap.Width, bitmap.Height), ImageLockMode.ReadOnly, PixelFormat.Format32bppArgb);
+                stride = bitmapData.Stride;
+                totalBytes = stride * altura;
+                byte[] pixelBuffer = new byte[totalBytes];
+                Marshal.Copy(bitmapData.Scan0, pixelBuffer, 0, totalBytes);
+                for (int j = 0; j < altura; j++)
+                {
+                    for (int k = 0; k < largura; k++)
+                    {
+                        offset = (j * stride) + (k * bytesPerPixel);
+                        pixels[k, j, i, 0] = pixelBuffer[offset + 2];
+                        pixels[k, j, i, 1] = pixelBuffer[offset + 1];
+                        pixels[k, j, i, 2] = pixelBuffer[offset];
+                        pixels[k, j, i, 3] = pixelBuffer[offset + 3];
+                        //Console.WriteLine($"RGB: ({k} {j} {i} - {pixels[k, j, i, 0]}, {pixels[k, j, i, 1]}, {pixels[k, j, i, 2]}, {pixels[k, j, i, 3]})");
+                    }
+                }
+                bitmap.UnlockBits(bitmapData);
+            }
+        }
+
     }
 }

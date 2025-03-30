b70811e826e7252ab9de95b23cbedace247e2a6b
Vitas <vitas.mellilo@gmail.com>
2025-03-29 21:38:26 -0300

    Aumento drástico na otimização. Necessário agora garantir que as threads sejam executadas sequencialmente

--- Controller/CapturaDeTela.cs
+++ Controller/CapturaDeTela.cs
@@ -18,13 +18,8 @@ namespace PoryGuard.Controller
         int frameRate;
         int boundsX;
         int boundsY;
-        int bytesPerPixel = 4;
-        int stride;
-        int totalBytes;
-        int offset;
         int aux_timer = 0;
         Size size;
-        short[,,,] pixels;
         Timer timer;
         List<Thread> coletor = new List<Thread>(); //Instancia uma lista de threads que coletarão bitmaps da tela
 
@@ -39,58 +34,38 @@ namespace PoryGuard.Controller
 
             altura = monitor.GetAltura();
             largura = monitor.GetLargura();
-            frameRate = 28; //Fixo em 20 para fins de otimização durante produção
-            //frameRate = monitor.GetFrameRate(); 
+            frameRate = monitor.GetFrameRate(); 
             boundsX = monitor.GetBoundsX();
             boundsY = monitor.GetBoundsY();
             size = new Size(largura, altura);
-            pixels = new short[largura, altura, frameRate, 4];
             timer = new Timer(CriaThread, null, 0, (Int32)(1000 / frameRate)); //timer para que a criação das threads
         }                                                                      //tenha a cadência da frameHate
         public void ColetaFrame(int i)
         {
-            Thread.Sleep(1000); //Pausa para a execução das threads não interferir na latência da criação das mesmas
             Stopwatch sw = new Stopwatch();
-            //while (true)
-            //{
+            while (true)
+            {
                 sw.Restart();
-                sw.Start();
-                BitmapData bitmapData;
                 using (Bitmap bitmap = new Bitmap(largura, altura))
                 {
                     using (Graphics g = Graphics.FromImage(bitmap))
                     {
                         g.CopyFromScreen(boundsX, boundsY, 0, 0, size, CopyPixelOperation.SourceCopy);
+                        
                     }
-                    bitmapData = bitmap.LockBits(new Rectangle(0, 0, bitmap.Width, bitmap.Height), ImageLockMode.ReadOnly, PixelFormat.Format32bppArgb);
-                    stride = bitmapData.Stride;
-                    totalBytes = stride * altura;
-                    byte[] pixelBuffer = new byte[totalBytes];
-                    Marshal.Copy(bitmapData.Scan0, pixelBuffer, 0, totalBytes);
-                    for (int j = 0; j < altura; j++)
-                    {
-                        for (int k = 0; k < largura; k++)
-                        {
-                            offset = (j * stride) + (k * bytesPerPixel);
-                            pixels[k, j, i, 0] = pixelBuffer[offset + 2];
-                            pixels[k, j, i, 1] = pixelBuffer[offset + 1];
-                            pixels[k, j, i, 2] = pixelBuffer[offset];
-                            pixels[k, j, i, 3] = pixelBuffer[offset + 3];
-                            //Console.WriteLine($"RGB: ({k} {j} {i} - {pixels[k, j, i, 0]}, {pixels[k, j, i, 1]}, {pixels[k, j, i, 2]}, {pixels[k, j, i, 3]})");
-                        }
-                    }
-                    bitmap.UnlockBits(bitmapData);
+                    bitmap.Save(i.ToString()+"screenshot.png", System.Drawing.Imaging.ImageFormat.Png);
                 }
                 sw.Stop();
                 if (sw.ElapsedMilliseconds < 1000)
                     Thread.Sleep(1000 - (Int32)sw.ElapsedMilliseconds);
                 Console.WriteLine(sw.ElapsedMilliseconds);
-            //}
+            }
         }
         public void CriaThread(object obj) //Função invocada pelo Timer
         {
+            Thread.Sleep(5000); //pausa para sincronização das threads
             int i = aux_timer;
-            if (aux_timer >= frameRate)
+            if (i >= frameRate)
             {
                 timer.Dispose();
                 return;
